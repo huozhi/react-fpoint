2022b17f1e42335ec73c1f11acce5a1ab880b88b
Jiachi Liu <inbox@huozhi.im>
2021-01-16 19:11:22 +0800

    example: tap

--- example/app.jsx
+++ example/app.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import ScrubExample from './scrub-example'
+import TapExample from './tap-example'
 
 function App() {
+  const [tab, setTab] = useState(location.hash.replace(/#/, '') || 'scrub')
   return (
     <div className='app'>
       <h1>React Fpoint</h1>
       <p>A set of react components make mouse and touch experience easier</p>
-      <ScrubExample />
+      <nav>
+        {['scrub', 'tap'].map(tab => (
+          <span 
+            className='tab' 
+            key={tab} 
+            onClick={() => { setTab(tab); location.hash = `#${tab}` }}
+          >
+            {`> ${tab}`}
+          </span>
+        ))}
+      </nav>
+      {tab === 'scrub' && <ScrubExample />}
+      {tab === 'tap' && <TapExample />}
     </div>
   )
 }
